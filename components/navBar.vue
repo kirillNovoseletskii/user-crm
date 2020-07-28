1d7767ed4ef1850a9d5c1009e91ce4eceffbb7f2
Kirill <kirill.novoseletskii2003@gmail.com>
2020-07-28 13:13:58 +0300

    add Back button, betta adaptive

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,23 +1,34 @@
 <template>
     <v-main>
         <v-toolbar extended dense color="gray" class="accent">
+        <v-app-bar-nav-icon @click="vOn" class="d-flex d-md-none"></v-app-bar-nav-icon>
+        <v-navigation-drawer
+            v-model="show"
+            absolute
+            right
+            :permanent="false"
+        />
         <v-toolbar-title>Ayax Test Task</v-toolbar-title>
         <template v-slot:extension>
-        <v-tabs
-          v-model="tabs"
-          centered
-          color='white'
-        >
-          <v-tab
-                v-for="l in links" :key="l.to"
-                @click="tabCl(l.to)"
-            >
-            {{ l.text }}
-          </v-tab>
-        </v-tabs>
+        <v-row class="d-none d-md-flex">
+
+            <v-col>
+                <v-tabs 
+                v-model="tabs"
+                color='white'
+                >
+                <v-tab
+                        v-for="l in links" :key="l.to"
+                        @click="tabCl(l.to)"
+                    >
+                    {{ l.text }}
+                </v-tab>
+                </v-tabs>
+            </v-col>
+        </v-row>
         </template>
         <v-spacer></v-spacer>
-        <v-row class='mt-5'>
+        <v-row class='mt-5' style="transform: translateX(30%)">
             <v-col md='2'>
             <v-autocomplete
                 append-icon=''
@@ -51,6 +62,9 @@
                 <v-btn @click="search">
                     <v-icon>mdi-card-search</v-icon>
                 </v-btn>
+                <v-btn @click="back">
+                    <v-icon>mdi-chevron-left-box</v-icon>
+                </v-btn>
             </v-col>
         </v-row>
 
@@ -65,6 +79,7 @@ export default {
     data() {
         return {
             rieltor: {},
+            show: false,
             tabs: null,
             width: 0,
             links: [
@@ -88,6 +103,16 @@ export default {
             const serchedRielt = this.data.filter(i => i.firstName==this.rieltor.firstName && i.lastName==this.rieltor.lastName && i.subdivision[0].name==this.rieltor.sub)
             this.writeSearch(serchedRielt)
             this.rieltor={}
+            this.$router.push('/table')
+        },
+        back(){
+            const serchedRielt = []
+            this.writeSearch(serchedRielt)
+            this.rieltor={}
+            this.$router.push('/table')
+        },
+        vOn(){
+            this.show = !this.show
         }
     },
     computed: {
